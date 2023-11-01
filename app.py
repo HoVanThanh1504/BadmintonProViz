aa2f4ae61756277c50861e61087e686f400362d3
Ho Van Thanh <toilathanh1504qn@gmail.com>
2023-11-01 15:18:52 -0400

    update UI for the website

--- app.py
+++ app.py
@@ -1,97 +1,87 @@
 from flask import Flask, render_template, request
 import plotly.graph_objects as go
 import plotly.express as px
-from load_data import top_10_men_singles, top_10_women_singles
+from load_data import set_of_players, df, group_player_winratio_by_year, group_player_matchscore_by_year, match_duration_by_year
 app = Flask(__name__)
 
-# Define a dictionary to store player statistics
-
-player_statistics = {
-    # "Player1": "Player A",
-    # "Player2": "Player B",
-    # "Nationality1": "Nation A",
-    # "Nationality2": "Nation B",
-    # "ScoreSet1": "21-18",
-    # "ScoreSet2": "19-21",
-    # "ScoreSet3": "21-17",
-    # "LongestPoint1": 35,
-    # "LongestPoint2": 42,
-    # "MatchPoint1": 5,
-    # "MatchPoint2": 7,
-    # "HeadToHead": "Player A leads 5-2",
-    # "Year": 2023,
-}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', players=set_of_players)
 
 
-@app.route('/type', methods=['GET', 'POST'])
-def display():
-    men_input_disabled = True  # Initially, the text input is disabled
-    women_input_disabled = True
-    if request.method == 'POST':
-        selected_option = request.form.get('category')
-        if selected_option == 'MS':
-            men_input_disabled = False
-        else:
-            women_input_disabled = False
-    return render_template('comparison.html', match_type=selected_option, men_input_disabled=men_input_disabled,
-                           women_input_disabled=women_input_disabled,
-                           men_players=top_10_men_singles, women_players=top_10_women_singles)
-
-
-@app.route('/compare', methods=['GET', 'POST'])
+@app.route('/display', methods=['GET', 'POST'])
 def display_stats():
-    def visualize(dataframe):
-        pass
+    player = request.form.get('player')
 
-    player_men_1 = request.form.get('playerm1')
-    player_men_2 = request.form.get('playerm2')
-    player_women_1 = request.form.get('playerw1')
-    player_women_2 = request.form.get('playerw2')
+    win_ratio = group_player_winratio_by_year(df, player)
+    match_score = group_player_matchscore_by_year(df, player)
+    match_duration = match_duration_by_year(df, player)
 
+    fig_win_loss = go.Figure()
+    fig_win_loss.add_trace(go.Scatter(x=win_ratio['Y'], y=win_ratio['%win_loss'], mode='lines+markers', name='% Win',
+                                      text=win_ratio["Match play"], textposition="top center",
+                                      hovertext=[f'Year: {y}<br>%win_loss: {win_loss}<br>Match play: {match_play}' for
+                                                 y, win_loss, match_play in
+                                                 zip(win_ratio['Y'], win_ratio['%win_loss'], win_ratio['Match play'])]
+                                      ))
+    fig_win_loss.update_layout(title=dict(text='Win-Loss Percentage Over Time', x=0.5, y=0.95),
+                               xaxis_title='Year', yaxis_title='% Win-Loss')
 
-    # stats_player1 = player_statistics.get(player1, {})
-    # stats_player2 = player_statistics.get(player2, {})
+    # fig_points_scored = go.Figure()
 
-    # Extracting the statistics and player info
-    # player1_stats = [stats_player1['Stat1'], stats_player1['Stat2']]
-    # player2_stats = [stats_player2['Stat1'], stats_player2['Stat2']]
-    # metric_labels = ['Stat1', 'Stat2']
-
-    # Create the bar chart using Plotly
-    # fig = go.Figure()
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set1'], mode='lines+markers', name='Set 1'))
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set2'], mode='lines+markers', name='Set 2'))
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set3'], mode='lines+markers', name='Set 3'))
     #
-    # fig.add_trace(go.Bar(
-    #     x=[player1, player2],
-    #     y=player1_stats,
-    #     name=player1,
-    #     text=['Country: ' + stats_player1['Country'] + '<br>Category: ' + stats_player1['Category']] * len(metric_labels),
-    #     hoverinfo='text+y',
-    #     offsetgroup=0
-    # ))
-    #
-    # fig.add_trace(go.Bar(
-    #     x=[player1, player2],
-    #     y=player2_stats,
-    #     name=player2,
-    #     text=['Country: ' + stats_player2['Country'] + '<br>Category: ' + stats_player2['Category']] * len(metric_labels),
-    #     hoverinfo='text+y',
-    #     offsetgroup=1
-    # ))
-    #
-    # fig.update_layout(
-    #     title='Comparison of Player Statistics',
-    #     xaxis=dict(title='Players'),
-    #     yaxis=dict(title='Statistics'),
-    #     barmode='group'
+    # # Add lines for opponent's set scores
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set1_op'], mode='lines+markers',
+    #                             name='Opponent Set 1'))
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set2_op'], mode='lines+markers',
+    #                             name='Opponent Set 2'))
+    # fig_points_scored.add_trace(go.Scatter(x=match_score['Y'], y=match_score['set3_op'], mode='lines+markers',
+    #                             name='Opponent Set 3'))
+
+    # Customize the layout
+    # fig_points_scored.update_layout(
+    #     title='Player vs. Opponent Set Scores Over Time',
+    #     xaxis_title='Year',
+    #     yaxis_title='Score',
     # )
+    fig_points_scored = go.Figure()
 
-    # plot_html = fig.to_html(full_html=False)
+    # Add bars for player's set scores
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set1'], name='Set 1 (Player)'))
 
-    return render_template('displayStat.html')
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set2'], name='Set 2 (Player)'))
+
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set3'], name='Set 3 (Player)'))
+
+    # Add bars for opponent's set scores
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set1_op'], name='Set 1 (Opponent)'))
+
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set2_op'], name='Set 2 (Opponent)'))
+    fig_points_scored.add_trace(go.Bar(x=match_score['Y'], y=match_score['set3_op'], name='Set 3 (Opponent)'))
+
+    # Customize the layout
+    fig_points_scored.update_layout(
+        title=dict(text='Player vs. Opponent Set Scores Over Time', x=0.5, y=0.95), xaxis_title='Year',
+        yaxis_title='Score', barmode='group'  # Display bars grouped for each year
+    )
+
+    fig_match_duration = go.Figure()
+    fig_match_duration.add_trace(go.Scatter(x=match_duration['Y'], y=match_duration['Duration'], mode='lines+markers',
+                                            name='Match Duration',
+                                            hovertext=[f'Duration: {duration} minute'
+                                                       for duration in match_duration['Duration']]
+                                            ))
+    fig_match_duration.update_layout(title=dict(text='Match Duration Over Time', x=0.5, y=0.95), xaxis_title='Year',
+                                     yaxis_title='Match Duration (minutes)')
+
+    return render_template('displayStat.html', plot_win_loss=fig_win_loss.to_html(full_html=False),
+                           plot_points_scored=fig_points_scored.to_html(full_html=False),
+                           plot_match_duration=fig_match_duration.to_html(full_html=False))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- load_data.py
+++ load_data.py
@@ -1,7 +1,5 @@
 import json
 import pandas as pd
-import numpy as np
-import plotly.express as px
 
 # Load data from JSON file
 path = './tournament_dataset.json'
@@ -16,12 +14,13 @@ for line in data:
 
 # Create a DataFrame from the extracted data
 df = pd.DataFrame.from_dict(match_statistic)
-df.set_index('Y')
+
 
 # Function to split match score
 def split_score(df, winner):
     num_rows = df.shape[0]
-    P1_T1_S1, P2_T2_S1, P1_T1_S2, P2_T2_S2, P1_T1_S3, P2_T2_S3 = [[0] * num_rows for _ in range(6)]
+    P1_T1_S1, P1_T1_S2, P1_T1_S3 = [[21] * num_rows for _ in range(3)]
+    P2_T2_S1, P2_T2_S2, P2_T2_S3 = [[0] * num_rows for _ in range(3)]
     i = 0
     for row in df.MatchScore: # row: 21-12, 12-21, 21-7
         row = row.split(",")
@@ -56,7 +55,7 @@ def split_score(df, winner):
     df.drop(columns='MatchScore', inplace=True)
     return df
 
-# Convert certain columns to integer type
+
 df = split_score(df, 1)
 # Replace specific values in the DataFrame
 df = df.replace({'duration:': 0, '-': 0})
@@ -66,22 +65,106 @@ df = df.astype({"T": int, "P1/T1-TP": int, "P2/T2-TP": int, "P1/T1-L": int, "P2/
                 "P2-H2H": int, "P1/T1-S1": int, "P2/T2-S1": int, "P1/T1-S2": int, "P2/T2-S2": int, "P1/T1-S3": int,
                 "P2/T2-S3": int, 'P1/T1-S': int, 'P2/T2-S': int})
 
-top_10_men_singles = ['Viktor AXELSEN', 'Kento MOMOTA', 'Anders ANTONSEN', 'CHOU Tien Chen', 'Anthony Sinisuka GINTING', 'CHEN Long', 'LEE Zii Jia', 'Jonatan CHRISTIE', 'NG Ka Long Angus', 'KIDAMBI Srikanth']
-top_10_women_singles = ['TAI Tzu Ying', 'Akane YAMAGUCHI', 'CHEN Yu Fei', 'AN Se Young', 'Nozomi OKUHARA', 'Carolina MARIN', 'PUSARLA V. Sindhu', 'Ratchanok INTANON', 'HE Bing Jiao', 'Pornpawee CHOCHUWONG']
-df_men_singles = df[(df['P1'].str.lower()).isin(list(map(lambda x: x.lower(), top_10_men_singles)))]
-df_women_singles = df[(df['P1'].str.lower()).isin(list(map(lambda x: x.lower(), top_10_women_singles)))]
+set_of_players = set(df['P1'].unique()).union(set(df['P2'].unique()))
 
 
-def compare_2_single_players(dataframe, player1, player2):
-    df_2_players = dataframe[((dataframe['P1'].str.lower() == player1.lower()) &
-                             (dataframe['P2'].str.lower() == player2.lower())) |
-                             ((dataframe['P2'].str.lower() == player1.lower()) &
-                             (dataframe['P1'].str.lower() == player2.lower()))]
-    year = df_2_players.index
-    nation1, nation2 = df_2_players[df['P1'] == player1]['P1-C'], df_2_players[df['P2'] == player2]['P2-C']
-    player1_h2h, player2_h2h = df_2_players['P1-H2H'], df_2_players['P2-H2H']
-    scores = dataframe['P1/T1-S1', 'P2/T2-S1', 'P1/T1-S2', 'P2/T2-S2', 'P1/T1-S3', 'P2/T2-S3']
-    longest_point = dataframe['P1/T1-LP', 'P2/T2-LP']
-    match_point = dataframe['P1/T1-GP', 'P2/T2-GP']
-    return year, nation1, nation2, player1_h2h, player2_h2h, scores, longest_point, match_point
+def group_player_winratio_by_year(df, player):
+    # Filter the DataFrame for matches where the player won (P1) or lost (P2)
+    df_player_win = df[df['P1'].str.lower() == player.lower()]
+    df_player_lose = df[df['P2'].str.lower() == player.lower()]
+
+    # Group and count the wins and losses by year
+    win_by_year = df_player_win.groupby(['Y'])['P1'].count().reset_index()
+    lose_by_year = df_player_lose.groupby(['Y'])['P2'].count().reset_index()
+
+    # Merge the win and loss DataFrames on 'Y' (Year) with an outer join to include all years
+    merge_df = win_by_year.merge(lose_by_year, on='Y', how='outer')
+
+    # Fill missing values with 0 to handle years with no wins or losses
+    merge_df = merge_df.fillna(0)
+
+    # Calculate the win-loss ratio and total match play for each year
+    merge_df['%win_loss'] = round(merge_df['P1'] / (merge_df['P1'] + merge_df['P2']) * 100, 2)
+    merge_df['Match play'] = merge_df['P1'] + merge_df['P2']
+    merge_df = merge_df.astype({'Match play': int})
+    # Return a DataFrame with the desired columns
+    return merge_df[['Y', '%win_loss', 'Match play']]
+
+
+def group_player_matchscore_by_year(df, player):
+    # Filter the DataFrame for matches where the player won (P1) or lost (P2)
+    df_player_win = df[df['P1'].str.lower() == player.lower()]
+    df_player_lose = df[df['P2'].str.lower() == player.lower()]
+
+    # Group and sum the point scores for each set by year for wins and losses
+    point_in_win_match_by_year = df_player_win.groupby(['Y'])[['P1/T1-S1', 'P1/T1-S2', 'P1/T1-S3']].sum().reset_index()
+    point_in_lose_match_by_year = df_player_lose.groupby(['Y'])[
+        ['P2/T2-S1', 'P2/T2-S2', 'P2/T2-S3']].sum().reset_index()
+
+    opponent_point_in_win_match_by_year = df_player_win.groupby(['Y'])[
+        ['P2/T2-S1', 'P2/T2-S2', 'P2/T2-S3']].sum().reset_index()
+    opponent_point_in_lose_match_by_year = df_player_lose.groupby(['Y'])[
+        ['P1/T1-S1', 'P1/T1-S2', 'P1/T1-S3']].sum().reset_index()
+    # Merge the point scores DataFrames on 'Y' with an outer join to ensure all years are included
+    merge_df = point_in_win_match_by_year.merge(point_in_lose_match_by_year, on='Y', how='outer')
+    merge_opponent_df = opponent_point_in_win_match_by_year.merge(opponent_point_in_lose_match_by_year, on='Y',
+                                                                  how='outer')
+    player_vs_opponent = merge_df.merge(merge_opponent_df, on='Y', how='outer')
+    # Fill missing values with 0 to handle years with no wins or losses
+    player_vs_opponent = player_vs_opponent.fillna(0)
+
+    # Group and count the total number of matches played by year for wins and losses
+    total_match_win = df_player_win.groupby(['Y'])['Winner'].count().reset_index()
+    total_match_lose = df_player_lose.groupby(['Y'])['Winner'].count().reset_index()
+
+    # Merge the match counts DataFrames on 'Y' with an outer join to ensure all years are included
+    merge_match = total_match_win.merge(total_match_lose, on='Y', how='outer')
+
+    # Fill missing values with 0 to handle years with no wins or losses
+    merge_match = merge_match.fillna(0)
+
+    # Calculate the total match play for each year and replace 0 with 1 to avoid division by zero
+    merge_match['MatchPlay'] = merge_match['Winner_x'] + merge_match['Winner_y']
+    merge_match['MatchPlay'] = merge_match['MatchPlay'].replace(0, 1)
+
+    # Calculate the total scores for each set by adding wins and losses
+    player_vs_opponent['set1'], player_vs_opponent['set1_op'] = player_vs_opponent['P1/T1-S1_x'] + player_vs_opponent[
+        'P2/T2-S1_x'], player_vs_opponent['P1/T1-S1_y'] + player_vs_opponent['P2/T2-S1_y']
+    player_vs_opponent['set2'], player_vs_opponent['set2_op'] = player_vs_opponent['P1/T1-S2_x'] + player_vs_opponent[
+        'P2/T2-S2_x'], player_vs_opponent['P1/T1-S2_y'] + player_vs_opponent['P2/T2-S2_y']
+    player_vs_opponent['set3'], player_vs_opponent['set3_op'] = player_vs_opponent['P1/T1-S3_x'] + player_vs_opponent[
+        'P2/T2-S3_x'], player_vs_opponent['P1/T1-S3_y'] + player_vs_opponent['P2/T2-S3_y']
+
+    # Normalize the set scores by dividing by the total match play
+    player_vs_opponent.iloc[:, 1:] = player_vs_opponent.iloc[:, 1:].div(merge_match['MatchPlay'].values, axis=0).round(2)
+    return player_vs_opponent[['Y', 'set1', 'set2', 'set3', 'set1_op', 'set2_op', 'set3_op']]
+
+
+def match_duration_by_year(df, player):
+    df_player_win = df[df['P1'].str.lower() == player.lower()]
+    df_player_lose = df[df['P2'].str.lower() == player.lower()]
+    win_match_duration = df_player_win.groupby(['Y'])[['T']].sum().reset_index()
+    loss_match_duration = df_player_lose.groupby(['Y'])[['T']].sum().reset_index()
+    merge_df = win_match_duration.merge(loss_match_duration, on='Y', how='outer')
+
+    # Fill missing values with 0 to handle years with no wins or losses
+    merge_df = merge_df.fillna(0)
+
+    # Group and count the total number of matches played by year for wins and losses
+    total_match_win = df_player_win.groupby(['Y'])['Winner'].count().reset_index()
+    total_match_lose = df_player_lose.groupby(['Y'])['Winner'].count().reset_index()
+
+    # Merge the match counts DataFrames on 'Y' with an outer join to ensure all years are included
+    merge_match = total_match_win.merge(total_match_lose, on='Y', how='outer')
+
+    # Fill missing values with 0 to handle years with no wins or losses
+    merge_match = merge_match.fillna(0)
+
+    # Calculate the total match play for each year and replace 0 with 1 to avoid division by zero
+    merge_match['MatchPlay'] = merge_match['Winner_x'] + merge_match['Winner_y']
+    merge_match['MatchPlay'] = merge_match['MatchPlay'].replace(0, 1)
+
+    merge_df['Duration'] = merge_df['T_x'] + merge_df['T_y']
+    merge_df.iloc[:, 1:] = merge_df.iloc[:, 1:].div(merge_match['MatchPlay'].values, axis=0).round(2)
+    return merge_df[['Y', 'Duration']]
 
